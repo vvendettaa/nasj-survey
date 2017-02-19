01e0fb38bb2ede5c16703a574e28ea0be2f4cf1b
vvendettaa <vvendettaaa@gmail.com>
2017-02-19 18:02:00 +0800

    try fix prod 1.4

--- resources/views/vendor/backpack/crud/columns/select.blade.php
+++ resources/views/vendor/backpack/crud/columns/select.blade.php
@@ -1,7 +1,7 @@
 {{-- single relationships (1-1, 1-n) --}}
 <td>
 	<?php
-	die(print_r( $entry->$column['entity']->$column['attribute']));
+	die(print_r( $entry));
 	//die(print_r($entry->$column['entity']->$column['attribute']));
 		if ($entry->$column['entity']->$column['attribute']) {
 	    	die(print_r( $entry->$column['entity']->$column['attribute']));
